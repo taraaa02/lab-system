7b55fccefc612a19a88ea9af939315016d4ff851
Dwiholqitahara02_ <tahararara333@gmail.com>
2026-04-14 23:49:43 +0700

    simpan

---
+++ login.c
@@ -0,0 +1,30 @@
+#include "header.h"
+
+int login(char *username, char *role, int argc, char *argv[]) {
+    Akun akun[100];
+    int jumlah = 0;
+    char inputUser[50], inputPass[50];
+
+    bacaAkun(akun, &jumlah);
+
+    printf("\n=== LOGIN ===\n");
+    printf("Username: ");
+    scanf("%s", inputUser);
+    printf("Password: ");
+    scanf("%s", inputPass);
+
+    for (int i = 0; i < jumlah; i++) {
+        if (strcmp(akun[i].username, inputUser) == 0 &&
+            strcmp(akun[i].password, inputPass) == 0) {
+
+            strcpy(username, akun[i].username);
+            strcpy(role, akun[i].role);
+
+            printf("Login berhasil sebagai %s\n", role);
+            return 1;
+        }
+    }
+
+    printf("Login gagal\n");
+    return 0;
+}
