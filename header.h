f6e21b378dd5a6d5a0a06d82199d863afbc79f58
Dwiholqitahara02_ <tahararara333@gmail.com>
2026-04-15 13:12:59 +0700

    simpan header

--- header.h
+++ header.h
@@ -5,6 +5,7 @@
 #include <string.h>
 
 // ===== STRUCT =====
+
 typedef struct {
     char username[50];
     char password[50];
@@ -12,31 +13,42 @@ typedef struct {
 } Akun;
 
 typedef struct {
+    int id;
     char nama[50];
     int stok;
 } Alat;
 
 typedef struct {
     char username[50];
-    char namaAlat[50];
+    int id_alat;
+    char nama_alat[100];
+    char tanggal[20];
+    int dikembalikan;
 } Pinjam;
 
 // ===== LOGIN =====
+
 int login(char *username, char *role, int argc, char *argv[]);
 
 // ===== FILE =====
+
 void bacaAkun(Akun akun[], int *jumlah);
 void bacaAlat(Alat alat[], int *jumlah);
-void simpanAlat(Alat alat[], int jumlah);
+void bacaPinjam(Pinjam pinjam[], int *jumlah);
+
+void tulisAlat(Alat alat[], int jumlah);
+void tulisPinjam(Pinjam pinjam[], int jumlah);
 
 // ===== ALAT =====
+
 void tampilAlat();
 void tambahAlat();
 void hapusAlat();
 
 // ===== PINJAM =====
-void pinjamAlat(char username[]);
-void lihatPinjaman(char username[]);
-void kembalikanAlat(char username[]);
+
+void pinjamAlat(char *username);
+void lihatPinjaman(char *username);
+void kembalikanAlat(char *username);
 
 #endif
